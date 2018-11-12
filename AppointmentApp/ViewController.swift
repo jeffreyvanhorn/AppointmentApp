0f34789575c79ea01e348a7329275fac285cbf47
Jeffrey Van Horn <jeffreyvanhorn@Jeffreys-MacBook-Pro.local>
2018-11-11 23:58:29 -0800

    Got the segue from the the main login page to the main tab bar view working

--- AppointmentApp/ViewController.swift
+++ AppointmentApp/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController, UITextFieldDelegate{
 
@@ -37,5 +38,34 @@ class ViewController: UIViewController, UITextFieldDelegate{
         textField.resignFirstResponder();
         return true;
     }
+    
+    //function to try to log the user in when the login button is clicked
+    @IBAction func loginButtonClick(_ sender: Any) {
+        //get and validate the input from the user
+        guard let email = emailTextField?.text, let password = passwordTextField?.text else{
+            return
+        }
+        
+        //perform more advanced input validation
+        
+        //try to sign in the user
+        //some of this code came from ray wenderlich firebase tutorial
+        Auth.auth().signIn(withEmail: email, password: password){
+            user, error in
+            if let error = error, user == nil {
+                let alert = UIAlertController(title: "Sign In Failed",
+                                              message: error.localizedDescription,
+                                              preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                
+                self.present(alert, animated: true, completion: nil)
+            }else{
+                //the login was successful and we want to proceed and perform the segue
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+    
 }
 
